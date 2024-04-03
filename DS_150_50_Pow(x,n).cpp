6306f94db1ed02b5b3535b7eb9a0c18e50501c3a
Saurabh <sygirme13@gmail.com>
2024-04-03 23:28:58 +0530

    DS_Interview_150 day 17 maths

---
+++ DS_150_50_Pow(x,n).cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+
+    double solve(double x, long int n)
+    {
+        if(n == 0)
+        {
+            return 1;
+        }
+
+        if(n < 0)
+        {
+            // recursive call
+            return solve(1/x, -n);
+        }
+
+        if(n % 2 == 0)
+        {
+            // recursive call
+            return solve(x*x, n/2);
+        }
+        else
+        {
+            // recursive call
+            return x*solve(x*x, (n-1)/2);
+        }
+    }
+
+    double myPow(double x, int n) 
+    {
+        return solve(x, long int n);    
+    }
+};
