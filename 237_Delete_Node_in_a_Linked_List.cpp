2047bd2bb1b8bae06b81f7c0555c6462951e94b3
Saurabh <sygirme13@gmail.com>
2024-05-05 23:14:41 +0530

    day 62 Linked List

---
+++ 237_Delete_Node_in_a_Linked_List.cpp
@@ -0,0 +1,24 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    void deleteNode(ListNode* node) 
+    {
+        // store the value of next node in current node
+        node -> val = node -> next -> val;
+        
+        // point to (next to next) node's address 
+        node -> next = node -> next -> next;    
+    }
+};
+
+
+// basically this is just overwriting of values and the node address
+// or say we are duplicating node->next in node and then pointing node to node->next->next
+// so basically we are removing node->next not node from given list
