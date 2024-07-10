da4588020e9e072924f94b7bba20cc6cbf9fed07
Saurabh <sygirme13@gmail.com>
2024-07-10 23:15:47 +0530

    Day 128 array using brute force(optimal)

---
+++ 1598_Crawler_Log_Folder_III.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int minOperations(vector<string>& logs) 
+    {
+        int depth = 0;
+
+        for(auto& log : logs)
+        {
+            if(log == "../")
+            {
+                depth = max(0, depth-1);
+            }
+            else if(log == "./")
+            {
+                continue;
+            }
+            else
+            {
+                depth++;
+            }
+        }
+
+
+        return depth;
+    }
+};
