92b67915ee21bf988e075b102757e27f6c040942
Saurabh <sygirme13@gmail.com>
2024-07-10 23:16:00 +0530

    Day 128 array using stack

---
+++ 1598_Crawler_Log_Folder_IV.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int minOperations(vector<string>& logs) 
+    {
+        stack<string> stk;
+
+        for(auto& log : logs)
+        {
+            if(log == "../")
+            {
+                if(!stk.empty())
+                {
+                    stk.pop();
+                }
+            }
+            else if(log != "./")
+            {
+                stk.push(log);;
+            }
+        }
+  
+        return stk.size();
+    }
+};
