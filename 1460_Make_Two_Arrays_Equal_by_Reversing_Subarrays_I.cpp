f8f2ec9503a604a8b6d934643ffe2b71bfa99169
Saurabh <sygirme13@gmail.com>
2024-08-04 19:21:44 +0530

    day 152 array using Sort function

---
+++ 1460_Make_Two_Arrays_Equal_by_Reversing_Subarrays_I.cpp
@@ -0,0 +1,20 @@
+// T.C = O(nlogn)   S.C = O(1)
+
+class Solution {
+public:
+    bool canBeEqual(vector<int>& target, vector<int>& arr) 
+    {
+        sort(arr.begin(), arr.end());
+        sort(target.begin(), target.end());    
+    
+        for(int i = 0; i < target.size(); i++)
+        {
+            if(arr[i] != target[i])
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
+};
