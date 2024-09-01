42a1e7e21b0b62e618489e6a19319744aeb6aa0f
Saurabh <sygirme13@gmail.com>
2024-09-01 20:10:08 +0530

    day 181 array matrix using simulation

---
+++ 2022_Convert_1D_Array_Into_2D_Array_I.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    vector<vector<int>> construct2DArray(vector<int>& original, int m, int n) 
+    {
+        int s = original.size();
+
+        if(s != (m*n))
+        {
+            return {};
+        }
+
+        vector<vector<int>> matrix(m, vector<int>(n));
+
+        int k = 0;
+        for(int i = 0; i < m; i++)
+        {   
+            for(int j = 0; j < n; j++)
+            {
+                matrix[i][j] = original[k];
+                k++;
+            }
+        }
+
+        return matrix;
+    }
+};
